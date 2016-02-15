b2412f0c074b4169adfd93c077d6c98233e8092f
M <m-coding@users.noreply.github.com>
2016-02-14 23:37:04 -0800

    Retrieve hotels from Firebase

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,28 +1,128 @@
-/* app.js
+/* Las Vegas Diamon Hotels Map
 ----------------------------------*/
 
+var app = app || {};
 
-/* FIREBASE hotels.json
-----------------------------------*/
+app.HotelModel     = app.HotelModel || {};
+app.HotelViewModel = app.HotelViewModel || {};
+app.HotelView      = app.HotelView || {};
+app.MapView        = app.MapView || {};
+
+/**
+ * Callback function for Google Maps (see index.html)
+ * Runs after the maps api is loded.
+ */
+var initApp = function() {
+    app.hm  = new app.HotelModel();
+    app.hvm = new app.HotelViewModel();
+    app.hv  = new app.HotelView();
+    app.mv  = new app.MapView();
+
+    app.hvm.init();
+};
+
+var test = {};
+
+app.HotelModel = function() {
+    'use strict';
+
+    var self = this;
+
+    /**
+     * @typedef Hotels
+     * @type Object
+     * @property {string} name - Name of hotel
+     * @property {object} location - Latitude/Longitude
+     * @property {number} diamonds - Diamond rating of hotel
+     */
+
+};
 
 
-function getHotels() {
-    // Get our model data from Firebase server
-    var hotelsRef = new Firebase('https://crackling-heat-3113.firebaseio.com/hotels');
+app.HotelViewModel = function() {
+    'use strict';
 
-    // Read the data only once
-    /* https://www.firebase.com/docs/web/guide/retrieving-data.html#section-reading-once */
-    /* https://www.firebase.com/docs/web/api/query/once.html */
-    hotelsRef.once('value', function(dataSnapshot) {
-        // do some stuff once
-        console.log("initial data loaded!", Object.keys(dataSnapshot.val()));
-        console.log('--- FIREBASE dataSnapshot---');
-        console.log(dataSnapshot.val());
+    var self = this;
+    self.hotels = {};
 
-    }, function (errorObject) {
-        // code to handle read error
-        console.log("The read failed: " + errorObject.code);
-    });
+    /**
+     * Retrieves the hotels json from Firebase server
+     *
+     * @see https://www.firebase.com/docs/web/guide/retrieving-data.html#section-reading-once
+     * @return {Hotels} - Hotel data in json notation
+     */
+    self.init = function() {
+        console.log('---app.HotelViewModel.init---');
+
+        var hotelsRef = new Firebase('https://crackling-heat-3113.firebaseio.com/hotels');
+
+        // Read the data only once https://www.firebase.com/docs/web/api/query/once.html
+        hotelsRef.once('value', function(dataSnapshot) {
+
+        var hotels = dataSnapshot.val();
+
+        self.hotels = hotels;
+
+        app.mv.initMap(hotels);
+
+        }, function (errorObject) {
+            // TODO: display error to user
+            // TODO: return at least 5 hotels from local file?
+            console.log("The read failed: " + errorObject.code);
+        });
+        console.log('---END.HotelViewModel.init---');
+    };
 
 
-} // getHotels
+}; // HotelViewModel
+
+
+app.HotelView = function() {
+    'use strict';
+
+}; // HotelView
+
+
+app.MapView = function() {
+    'use strict';
+
+    var self = this;
+
+    /**
+     * Displays a Google Map
+     *
+     * @see https://developers.google.com/maps/documentation/javascript/reference
+     * @param {[type]} [varname] [description] // TODO
+     */
+    self.initMap = function(data) {
+        var mapOptions = {
+            disableDefaultUI: true,
+            center: {lat: 36.1070161, lng: -115.1798162},
+            zoom: 15
+            // 1: World
+            // 5: Landmass/continent
+            // 10: City
+            // 15: Streets
+            // 20: Buildings
+        }; // mapOptions
+
+        //var location = data.location;
+
+        var locationLatLng = {lat: 36.1066653, lng: -115.1790977};
+
+
+        console.log("-----app.MapView.initMap----");
+        console.dir(data);
+
+
+        var mapDiv = document.getElementById('map');
+
+        var map = new google.maps.Map(mapDiv, mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: locationLatLng,
+            map: map,
+            title: 'Hello World!'
+        }); // marker
+    }; // initMap
+}; // MapView
